ed7a9e7b16bee8439cdbda9574b493e9165612b0
YongTeng Huang <charliehuang1017@gmail.com>
2023-02-25 20:38:10 +0800

    Add Tree structure

---
+++ tree/tree.go
@@ -0,0 +1,50 @@
+package tree
+
+import "fmt"
+
+type Node struct {
+	value       int
+	left, right *Node
+}
+type Tree struct {
+	root *Node
+}
+
+func (t *Tree) PrintTree() {
+	fmt.Print("Tree : ")
+	printPreOrder(t.root)
+	fmt.Println()
+}
+
+func printPreOrder(n *Node) {
+	if n == nil {
+		return
+	}
+	fmt.Print(n.value, " ")
+	printPreOrder(n.left)
+	printPreOrder(n.right)
+}
+
+// LevelOrderBinaryTree
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+func LevelOrderBinaryTree(arr []int) *Tree {
+	tree := new(Tree)
+	tree.root = levelOrderBinaryTree(arr, 0, len(arr))
+	return tree
+}
+
+func levelOrderBinaryTree(arr []int, start int, size int) *Node {
+	curr := &Node{arr[start], nil, nil}
+
+	left := 2*start + 1
+	right := 2*start + 2
+
+	if left < size {
+		curr.left = levelOrderBinaryTree(arr, left, size)
+	}
+	if right < size {
+		curr.right = levelOrderBinaryTree(arr, right, size)
+	}
+	return curr
+}
---
+++ tree/tree_test.go
@@ -0,0 +1,12 @@
+package tree_test
+
+import (
+	algoTree "ds_algo/tree"
+	"testing"
+)
+
+func Test_Tree(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	tree := algoTree.LevelOrderBinaryTree(arr)
+	tree.PrintTree()
+}
